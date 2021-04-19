002a1072919ad5997d2f3a095a21aedbbaf80955
Cairo Mitchell-Acason <cairoacason.john@gmail.com>
2021-04-19 22:55:32 +1200

    Switched to party dot colours, added download svg and png

--- assets/main.js
+++ assets/main.js
@@ -59,6 +59,7 @@ function partyInfo(acronym, colour, seats) {
     tspan.setAttribute("dy", "1.2em");
     tspan.setAttribute("fill", colour);
     tspan.setAttribute("font-weight", "bold");
+    tspan.setAttribute("font-family", 'BlinkMacSystemFont,-apple-system,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",Helvetica,Arial,sans-serif');
 
     document.getElementById("partyinfo-texts").appendChild(tspan);
 }
@@ -127,4 +128,115 @@ function fillmap() {
             }
         }
     }   
+
+    SVGToPNG(document.getElementById('seatMapSVG').innerHTML);
 }
+
+function generateMapDownload(filename, text) {
+    const element = document.createElement("a");
+    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
+    element.setAttribute("download", filename);
+    
+    element.style.display = "none";
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+function svg2img(svg){
+    var svg64 = btoa(svg);
+    var b64start = 'data:image/svg+xml;base64,';
+    var image64 = b64start + svg64;
+    return image64;
+};
+
+ /**
+  * converts an svg string to base64 png using the domUrl
+  * @param {string} svgText the svgtext
+  * @param {number} [margin=0] the width of the border - the image size will be height+margin by width+margin
+  * @param {string} [fill] optionally backgrund canvas fill
+  * @return {Promise} a promise to the bas64 png image
+  */
+  var svgToPng = function (svgText, margin,fill) {
+    // convert an svg text to png using the browser
+    return new Promise(function(resolve, reject) {
+      try {
+        // can use the domUrl function from the browser
+        var domUrl = window.URL || window.webkitURL || window;
+        if (!domUrl) {
+          throw new Error("(browser doesnt support this)")
+        }
+        
+        // figure out the height and width from svg text
+        var match = svgText.match(/height=\"(\d+)/m);
+        var height = match && match[1] ? parseInt(match[1],10) : 200;
+        var match = svgText.match(/width=\"(\d+)/m);
+        var width = match && match[1] ? parseInt(match[1],10) : 200;
+        margin = margin || 0;
+        
+        // it needs a namespace
+        if (!svgText.match(/xmlns=\"/mi)){
+          svgText = svgText.replace ('<svg ','<svg xmlns="http://www.w3.org/2000/svg" ') ;  
+        }
+        
+        // create a canvas element to pass through
+        var canvas = document.createElement("canvas");
+        canvas.width = (height+margin*2) - 125;
+        canvas.height = (width+margin*2) + 110;
+        var ctx = canvas.getContext("2d");
+        
+        
+        // make a blob from the svg
+        var svg = new Blob([svgText], {
+          type: "image/svg+xml;charset=utf-8"
+        });
+        
+        // create a dom object for that image
+        var url = domUrl.createObjectURL(svg);
+        
+        // create a new image to hold it the converted type
+        var img = new Image;
+        
+        // when the image is loaded we can get it as base64 url
+        img.onload = function() {
+          // draw it to the canvas
+          ctx.drawImage(this, margin, margin);
+          
+          // if it needs some styling, we need a new canvas
+          if (fill) {
+            var styled = document.createElement("canvas");
+            styled.width = canvas.width;
+            styled.height = canvas.height;
+            var styledCtx = styled.getContext("2d");
+            styledCtx.save();
+            styledCtx.fillStyle = fill;   
+            styledCtx.fillRect(0,0,canvas.width,canvas.height);
+            styledCtx.strokeRect(0,0,canvas.width,canvas.height);
+            styledCtx.restore();
+            styledCtx.drawImage (canvas, -10,0);
+            canvas = styled;
+          }
+          // we don't need the original any more
+          domUrl.revokeObjectURL(url);
+          // now we can resolve the promise, passing the base64 url
+          resolve(canvas.toDataURL());
+        };
+        
+        // load the image
+        img.src = url;
+        
+      } catch (err) {
+        reject('failed to convert svg to png ' + err);
+      }
+    });
+  };
+
+function SVGToPNG(svg) {
+    svgToPng(svg, 0, "#FFFFFF").then(png => {
+        document.getElementById("dlSeatMapPNG").setAttribute("download", "seatmap.png");
+        document.getElementById("dlSeatMapPNG").setAttribute("href", png);
+    });
+}
+
