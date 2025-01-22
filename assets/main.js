a408245e89d4d0c293804481613a12434e77f160
Carolina Mitchell <carolinamitchell@outlook.co.nz>
2025-01-23 04:00:53 +1300

    Update design of the generator, new bulma version, includes dark theme

--- assets/main.js
+++ assets/main.js
@@ -245,3 +245,38 @@ function maxNumberFix(id, min, max) {
     if (element > max) document.getElementById(id).value = max;
     if (element < min) document.getElementById(id).value = min;
 }
+
+// Get all dropdowns on the page that aren't hoverable.
+const dropdowns = document.querySelectorAll('.dropdown:not(.is-hoverable)');
+
+if (dropdowns.length > 0) {
+  // For each dropdown, add event handler to open on click.
+  dropdowns.forEach(function(el) {
+    el.addEventListener('click', function(e) {
+      e.stopPropagation();
+      el.classList.toggle('is-active');
+    });
+  });
+
+  // If user clicks outside dropdown, close it.
+  document.addEventListener('click', function(e) {
+    closeDropdowns();
+  });
+}
+
+/*
+ * Close dropdowns by removing `is-active` class.
+ */
+function closeDropdowns() {
+  dropdowns.forEach(function(el) {
+    el.classList.remove('is-active');
+  });
+}
+
+// Close dropdowns if ESC pressed
+document.addEventListener('keydown', function (event) {
+  let e = event || window.event;
+  if (e.key === 'Esc' || e.key === 'Escape') {
+    closeDropdowns();
+  }
+});
